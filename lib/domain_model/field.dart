923f5671759583cccdfd246c67aa831280d66bf5
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2021-09-18 17:48:14 +0200

    Add _name field to Field domain model

--- lib/domain_model/field.dart
+++ lib/domain_model/field.dart
@@ -1,14 +1,40 @@
 class Field {
+  static const int NAME_MAXIMUM_LENGTH = 64;
   late final String _id;
+  late String _name;
 
-  Field(String uuid) {
+  Field(String uuid, String name) {
+    /////////////////////////////////////////////////////////////////////////
+    // _id validation
     final regexp = RegExp(
         "[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}");
     if (!regexp.hasMatch(uuid)) {
       throw ArgumentError("_id must be a valid UUID");
     }
     this._id = uuid;
+    /////////////////////////////////////////////////////////////////////////
+    // _name validation
+    if (name.isEmpty) {
+      throw ArgumentError("_name cannot be empty");
+    }
+    if (name.length > NAME_MAXIMUM_LENGTH) {
+      throw ArgumentError("_name length cannot be greater than 64 character");
+    }
+    this._name = name;
   }
 
   String get id => _id;
+  String get name => _name;
+
+  set name(String name) {
+    /////////////////////////////////////////////////////////////////////////
+    // _name validation
+    if (name.isEmpty) {
+      throw ArgumentError("_name cannot be empty");
+    }
+    if (name.length > NAME_MAXIMUM_LENGTH) {
+      throw ArgumentError("_name length cannot be greater than 64 character");
+    }
+    this._name = name;
+  }
 }
--- test/domain_model/field_test.dart
+++ test/domain_model/field_test.dart
@@ -3,20 +3,64 @@ import 'package:study_without_pen_by_flutter/domain_model/field.dart';
 import 'package:uuid/uuid.dart';
 
 main() {
+  final uuid = Uuid();
   group("_id tests", () {
     test("_id is a valid UUID", () {
-      final uuid = Uuid();
-      expect(() => Field(""), throwsArgumentError);
-      expect(() => Field("whfw"), throwsArgumentError);
-      expect(() => Field(uuid.v4()), returnsNormally);
+      expect(() => Field("", "name"), throwsArgumentError);
+      expect(() => Field("whfw", "name"), throwsArgumentError);
+      expect(() => Field(uuid.v4(), "name"), returnsNormally);
     });
-
     test("_id has been assigned the correct value", () {
-      Uuid uuid = Uuid();
       String uuidString = uuid.v4();
-      final field = Field(uuidString);
+      final field = Field(uuidString, "name");
       final id = field.id;
       expect(uuidString, id);
+    });
+  });
+  group("_name tests", () {
+    test("_name isn't empty", () {
+      expect(() => Field(uuid.v4(), ""), throwsArgumentError);
+      expect(() {
+        final field = Field(uuid.v4(), "name");
+        field.name = "";
+      }, throwsArgumentError);
+    });
+    test("_name has length that is not greater than 64 character", () {
+      expect(() => Field(uuid.v4(), """
+    fffffffffffffffffffff
+    fffffffffffffffff
+    fffffffffffffffff
+    ffffffffffffffffff
+    ffffffffffffffffff
+    fffffffffffffffff
+    fffffffffffffffff
+    fffffffffffffffffff
+    fffffffffffffffffff
+    ffffffffffffffffff
+    """), throwsArgumentError);
+      expect(() {
+        final field = Field(uuid.v4(), "name");
+        field.name = """
+    fffffffffffffffffffff
+    fffffffffffffffff
+    fffffffffffffffff
+    ffffffffffffffffff
+    ffffffffffffffffff
+    fffffffffffffffff
+    fffffffffffffffff
+    fffffffffffffffffff
+    fffffffffffffffffff
+    ffffffffffffffffff
+    """;
+      }, throwsArgumentError);
+    });
+    test("_name has been assigned the correct value", () {
+      final field = Field(uuid.v4(), "name");
+      var name = field.name;
+      expect("name", name);
+      field.name = "name2";
+      name = field.name;
+      expect("name2", name);
     });
   });
 }
