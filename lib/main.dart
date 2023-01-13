870b38ae77df0844ef1ba54cfdc513c882d8797f
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2023-01-13 16:21:39 +0200

    Add Firebase Android app configuration files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:study_without_pen_by_flutter/firebase_options.dart';
 
 import 'app_runner.dart';
 
@@ -10,6 +11,6 @@ void main() async {
           runApp,
           WidgetsFlutterBinding.ensureInitialized,
           Firebase.initializeApp,
-          FirebaseOptions(apiKey: '', appId: '', messagingSenderId: '', projectId: ''))
+          DefaultFirebaseOptions.currentPlatform)
       .run());
 }
