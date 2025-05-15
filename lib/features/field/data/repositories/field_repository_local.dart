ddc6147e3599a4b0280237138733df1d001ae12f
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-05-15 05:51:11 +0300

    FieldRepositoryLocal: implement createField()

--- lib/features/field/data/repositories/field_repository_local.dart
+++ lib/features/field/data/repositories/field_repository_local.dart
@@ -1,9 +1,20 @@
+import 'package:drift/drift.dart';
+import 'package:study_without_pen_by_flutter/database/app_database.dart';
+import 'package:study_without_pen_by_flutter/database/fields_dao.dart';
 import 'package:study_without_pen_by_flutter/features/field/data/repositories/field_repository.dart';
 import 'package:study_without_pen_by_flutter/features/field/domain/models/field_entity.dart';
 
 class FieldRepositoryLocal implements FieldRepository {
+  FieldRepositoryLocal(this.fieldsDao);
+  FieldsDao fieldsDao;
   @override
   Future<int> create(FieldEntity fieldEntity) {
-    return Future.value(1);
+    return fieldsDao.create(FieldsCompanion(
+        userAccountId: Value(fieldEntity.userAccountId),
+        name: Value(fieldEntity.name),
+        creationAt: Value(fieldEntity.creationAt),
+        lastModificationAt: Value(fieldEntity.lastModificationAt),
+        usageCount: Value(fieldEntity.usageCount),
+        color: Value(fieldEntity.color)));
   }
 }
