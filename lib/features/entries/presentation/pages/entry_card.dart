13b5c362d0ce45ccec7b121be508f9291be5ac96
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2026-02-09 20:35:15 +0200

    EntryCard: display the score and wrongness

--- lib/features/entries/presentation/pages/entry_card.dart
+++ lib/features/entries/presentation/pages/entry_card.dart
@@ -10,139 +10,159 @@ class EntryCard extends StatelessWidget {
   final EntryEntity entry;
 
   @override
-  Widget build(BuildContext context) => Material(
-    child: Dismissible(
-      key: const Key('hi'),
-      child: InkWell(
-        child: Card(
-          elevation: 5,
-          margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-          child: ClipPath(
-            clipper: ShapeBorderClipper(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border(
-                  left: BorderSide(color: Colors.red.shade700, width: 7),
+  Widget build(BuildContext context) {
+    var sideColor = Colors.red.shade700;
+    if (entry.wrongness <= 0.1) {
+      sideColor = Colors.green;
+    } else if (entry.wrongness > 0.1 && entry.wrongness <= 0.4) {
+      sideColor = Colors.yellow;
+    }
+
+    return Material(
+      child: Dismissible(
+        key: const Key('hi'),
+        child: InkWell(
+          child: Card(
+            elevation: 5,
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: ClipPath(
+              clipper: ShapeBorderClipper(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Chip(
-                            label: Text(AppLocalizations.of(context)!.score),
-                            labelStyle: Theme.of(context).textTheme.labelSmall,
-                          ),
-                          Chip(
-                            label: Text(
-                              AppLocalizations.of(context)!.wrongness,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border(left: BorderSide(color: sideColor, width: 4)),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Chip(
+                              label: Text(
+                                '${AppLocalizations.of(context)!.score}: ${entry.score}',
+                              ),
+                              labelStyle: Theme.of(
+                                context,
+                              ).textTheme.labelSmall,
                             ),
-                            labelStyle: Theme.of(context).textTheme.labelSmall,
-                          ),
-                          Chip(
-                            label: Text(switch (entry.rank) {
-                              Rank.low => AppLocalizations.of(context)!.low,
-                              Rank.normal => AppLocalizations.of(
+                            Chip(
+                              label: Text(
+                                '${AppLocalizations.of(context)!.wrongness}: ${entry.wrongness}',
+                              ),
+                              labelStyle: Theme.of(
                                 context,
-                              )!.normal,
-                              Rank.important => AppLocalizations.of(
+                              ).textTheme.labelSmall,
+                            ),
+                            Chip(
+                              label: Text(switch (entry.rank) {
+                                Rank.low => AppLocalizations.of(context)!.low,
+                                Rank.normal => AppLocalizations.of(
+                                  context,
+                                )!.normal,
+                                Rank.important => AppLocalizations.of(
+                                  context,
+                                )!.important,
+                                Rank.vital => AppLocalizations.of(
+                                  context,
+                                )!.vital,
+                              }),
+                              labelStyle: Theme.of(
                                 context,
-                              )!.important,
-                              Rank.vital => AppLocalizations.of(context)!.vital,
-                            }),
-                            labelStyle: Theme.of(context).textTheme.labelSmall,
-                          ),
-                        ],
-                      ),
-                      PopupMenuButton<String>(
-                        itemBuilder: (_) => [
-                          const PopupMenuItem<String>(
-                            value: '',
-                            child: Text('hi'),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    key: Key('scoreRankQuestionSizedBox'),
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      Flexible(
-                        child: Center(
-                          child: SelectableText(
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.titleLarge!
-                                .copyWith(fontWeight: FontWeight.w600),
-                            entry.question,
+                              ).textTheme.labelSmall,
+                            ),
+                          ],
+                        ),
+                        PopupMenuButton<String>(
+                          itemBuilder: (_) => [
+                            const PopupMenuItem<String>(
+                              value: '',
+                              child: Text('hi'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      key: Key('scoreRankQuestionSizedBox'),
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: Center(
+                            child: SelectableText(
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.titleLarge!
+                                  .copyWith(fontWeight: FontWeight.w600),
+                              entry.question,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    key: Key('questionAnswerSizedBox'),
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      Flexible(
-                        child: Center(
-                          child: SelectableText(
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.bodyLarge,
-                            entry.answer,
+                      ],
+                    ),
+                    const SizedBox(
+                      key: Key('questionAnswerSizedBox'),
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: Center(
+                            child: SelectableText(
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.bodyLarge,
+                              entry.answer,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(key: Key('answerButtonsSizedBox'), height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Theme.of(
-                            context,
-                          ).colorScheme.onSecondary,
+                      ],
+                    ),
+                    const SizedBox(
+                      key: Key('answerButtonsSizedBox'),
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme.of(
+                              context,
+                            ).colorScheme.onSecondary,
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            style: Theme.of(context).textTheme.titleSmall,
+                            AppLocalizations.of(context)!.study,
+                          ),
                         ),
-                        onPressed: () {},
-                        child: Text(
-                          style: Theme.of(context).textTheme.titleSmall,
-                          AppLocalizations.of(context)!.study,
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme.of(
+                              context,
+                            ).colorScheme.onSecondary,
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            style: Theme.of(context).textTheme.titleSmall,
+                            AppLocalizations.of(context)!.test,
+                          ),
                         ),
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Theme.of(
-                            context,
-                          ).colorScheme.onSecondary,
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          style: Theme.of(context).textTheme.titleSmall,
-                          AppLocalizations.of(context)!.test,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
