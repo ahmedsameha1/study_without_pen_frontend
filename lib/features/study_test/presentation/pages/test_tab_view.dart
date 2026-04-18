9f0189d6852fb96acb62608434314b52221bc7c9
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2026-04-18 07:11:05 +0200

    TestTabView: Add CheckUserAnswer event when user taps check button

--- lib/features/study_test/presentation/pages/test_tab_view.dart
+++ lib/features/study_test/presentation/pages/test_tab_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../l10n/app_localizations.dart';
 import '../../../entries/domain/models/entry_entity.dart';
+import '../bloc/study_test_bloc.dart';
+import '../bloc/study_test_event.dart';
+import '../bloc/study_test_state.dart';
 
 class TestTabView extends StatefulWidget {
   const TestTabView({required this.entry, required this.count, super.key});
@@ -15,109 +19,127 @@ class TestTabView extends StatefulWidget {
 class _TestTabViewState extends State<TestTabView> {
   static const OutlineInputBorder _outlineInputBorder = OutlineInputBorder();
   late final ScrollController _scrollController;
+  late TextEditingController _userAnswerTextEditingController;
 
   @override
   void initState() {
+    _userAnswerTextEditingController = TextEditingController();
     _scrollController = ScrollController();
     super.initState();
   }
 
   @override
   void dispose() {
+    _userAnswerTextEditingController.dispose();
     _scrollController.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) => Center(
-    child: Scrollbar(
-      controller: _scrollController,
-      interactive: true,
-      thumbVisibility: true,
-      thickness: 15,
-      child: Padding(
-        key: const Key('tabPadding'),
-        padding: const EdgeInsetsDirectional.symmetric(
-          horizontal: 30,
-          vertical: 15,
-        ),
-        child: SingleChildScrollView(
-          controller: _scrollController,
-          child: Column(
-            children: [
-              SizedBox(
-                key: const Key('questionSizedBox'),
-                height: 200,
-                width: double.infinity,
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: _outlineInputBorder.borderSide.color,
-                      width: _outlineInputBorder.borderSide.width,
+  Widget build(
+    BuildContext context,
+  ) => BlocListener<StudyTestBloc, StudyTestState>(
+    listener: (BuildContext context, state) {
+      if (state.isUserAnswerCorrect) {
+        _userAnswerTextEditingController.clear();
+      }
+    },
+    child: Center(
+      child: Scrollbar(
+        controller: _scrollController,
+        interactive: true,
+        thumbVisibility: true,
+        thickness: 15,
+        child: Padding(
+          key: const Key('tabPadding'),
+          padding: const EdgeInsetsDirectional.symmetric(
+            horizontal: 30,
+            vertical: 15,
+          ),
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
+              children: [
+                SizedBox(
+                  key: const Key('questionSizedBox'),
+                  height: 200,
+                  width: double.infinity,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: _outlineInputBorder.borderSide.color,
+                        width: _outlineInputBorder.borderSide.width,
+                      ),
+                      borderRadius: _outlineInputBorder.borderRadius,
                     ),
-                    borderRadius: _outlineInputBorder.borderRadius,
-                  ),
-                  child: SingleChildScrollView(
-                    padding: const EdgeInsets.all(10),
-                    child: ConstrainedBox(
-                      constraints: const BoxConstraints(minHeight: 180),
-                      child: Center(
-                        child: Text(
-                          widget.entry.question,
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.bodyLarge,
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.all(10),
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(minHeight: 180),
+                        child: Center(
+                          child: Text(
+                            widget.entry.question,
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(key: Key('questionUserAnswerSizedBox'), height: 20),
-              SizedBox(
-                key: const Key('userAnswerSizedBox'),
-                height: 200,
-                child: TextFormField(
-                  style: Theme.of(context).textTheme.bodyLarge,
-                  expands: true,
-                  maxLines: null,
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.multiline,
-                  autofocus: true,
-                  autocorrect: false,
-                  enableSuggestions: false,
-                  textInputAction: TextInputAction.newline,
-                  decoration: InputDecoration(
-                    hintText: AppLocalizations.of(context)!.enterYourAnswer,
-                    contentPadding: const EdgeInsets.only(
-                      top: 20,
-                      bottom: 10,
-                      left: 10,
-                      right: 10,
+                const SizedBox(
+                  key: Key('questionUserAnswerSizedBox'),
+                  height: 20,
+                ),
+                SizedBox(
+                  key: const Key('userAnswerSizedBox'),
+                  height: 200,
+                  child: TextFormField(
+                    controller: _userAnswerTextEditingController,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                    expands: true,
+                    maxLines: null,
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.multiline,
+                    autofocus: true,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    textInputAction: TextInputAction.newline,
+                    decoration: InputDecoration(
+                      hintText: AppLocalizations.of(context)!.enterYourAnswer,
+                      contentPadding: const EdgeInsets.only(
+                        top: 20,
+                        bottom: 10,
+                        left: 10,
+                        right: 10,
+                      ),
+                      filled: true,
+                      border: const OutlineInputBorder(gapPadding: 0),
                     ),
-                    filled: true,
-                    border: const OutlineInputBorder(gapPadding: 0),
                   ),
                 ),
-              ),
-              const SizedBox(
-                key: Key('userAnswerCheckButtonSizedBox'),
-                height: 20,
-              ),
-              FilledButton(
-                onPressed: () {},
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [Text(AppLocalizations.of(context)!.check)],
-                      ),
-                    ),
-                    Text('${widget.count}'),
-                  ],
+                const SizedBox(
+                  key: Key('userAnswerCheckButtonSizedBox'),
+                  height: 20,
                 ),
-              ),
-            ],
+                FilledButton(
+                  onPressed: () => BlocProvider.of<StudyTestBloc>(
+                    context,
+                  ).add(CheckUserAnswer(_userAnswerTextEditingController.text)),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [Text(AppLocalizations.of(context)!.check)],
+                        ),
+                      ),
+                      Text('${widget.count}'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
